1f2abbadab1917ba734adae55a1130e2afd942ff
Lee Jeong Min <danmin20@khu.ac.kr>
2019-12-10 18:49:19 +0900

    Update checkAuction.js

--- checkAuction.js
+++ checkAuction.js
@@ -1,4 +1,4 @@
-//node-schedulr 패키지는 스케줄링이 노드 기반으로 작동하므로 노드가 종료되면 스케줄 예약도 같이 종료됨
+//node-schedule 패키지는 스케줄링이 노드 기반으로 작동하므로 노드가 종료되면 스케줄 예약도 같이 종료됨
 //이를 보완하기 위하여, 서버가 시작될 때 경매 시작 후 24시간이 지났지만 낙찰자는 없는 경매를 찾아서 낙찰자를 지정
 
 const { Good, Auction, User, sequelize } = require('./models');
@@ -10,14 +10,9 @@ module.exports = async () => {
     const targets = await Good.findAll({
       where: {
         [sequelize.Op.and]:[
-          {
-            soldId: {
-              [sequelize.Op.eq]:null
-            }
-          },
-        {
-          createdAt: { [sequelize.Op.lte]: yesterday }
-        }]
+          { soldId: {[sequelize.Op.eq]:null} },
+          { createdAt: { [sequelize.Op.lte]: yesterday } }
+        ]
       },
     });
     console.log(targets)
